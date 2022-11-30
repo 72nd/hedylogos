85b6a156d88c18fe03bfca71eccedf864b65a793
72nd <msg@frg72.com>
2022-11-30 18:16:55 +0100

    basic story structure added

--- model/story.go
+++ model/story.go
@@ -3,3 +3,22 @@
 // This package also handles the import and conversion from the more basic
 // result of an [github.com/hedylogos/graphml] import.l
 package model
+
+// Story contains a whole audio scenario containing the metadata and the
+// start nodes.
+type Story struct {
+	// Optional version number for a story. This is used to distinguish
+	// between different versions of a stroy.
+	Version string
+	// The optional name(s) of the author(s) of the story.
+	Author string
+	// Optional description of the content of the story.
+	Description string
+	// Languages supported by the story.
+	Languages Languages
+	// The start nodes of the story.
+	Starts []*Node
+	// The collection of all nodes in the story. Should be only used
+	// internally.
+	Nodes Nodes
+}
