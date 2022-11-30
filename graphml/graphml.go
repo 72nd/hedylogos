a1db90e8bf0920bc2c747e63b4a3eb3212547b31
72nd <msg@frg72.com>
2022-11-30 17:12:59 +0100

    basic structure of an graphml file implemented

--- graphml/graphml.go
+++ graphml/graphml.go
@@ -1,3 +1,4 @@
+// This package provides a very simple importer for any gr
 package graphml
 
 import (
@@ -11,19 +12,60 @@ type Key struct {
 	Name    string   `xml:"attr.name,attr"`
 	Type    string   `xml:"attr.type,attr"`
 	For     string   `xml:"for,attr"`
-	Default *Default `xml:"default"`
+	Default string   `xml:"default"`
 }
 
-type Default struct {
-	XMLName  xml.Name `xml:"default"`
-	XMLSpace string   `xml:"xml:space,attr"`
-	Body     string   `xml:",chardata"`
+type Language struct {
+	XMLName xml.Name `xml:"language"`
+	Name    string   `xml:"name,attr"`
+}
+
+type ShapeNode struct {
+	XMLName xml.Name `xml:"ShapeNode"`
+	Label   string   `xml:"NodeLabel"`
+}
+
+type ShapeData struct {
+	XMLName xml.Name `xml:"ShapeNode"`
+}
+
+type Data struct {
+	XMLName   xml.Name   `xml:"data"`
+	Key       string     `xml:"key,attr"`
+	XMLSpace  string     `xml:"xml:space,attr"`
+	Languages []Language `xml:"language"`
+	ShapeData ShapeData  `xml:"ShapeNode"`
+	Value     string     `xml:",innerxml"`
+}
+
+type Graph struct {
+	XMLName     xml.Name `xml:"graph"`
+	ID          string   `xml:"id,attr"`
+	EdgeDefault string   `xml:"edgedefault,attr"`
+	Data        []Data   `xml:"data"`
+	Nodes       []Node   `xml:"node"`
+	Edges       []Edge   `xml:"edge"`
+}
+
+type Node struct {
+	XMLName xml.Name `xml:"node"`
+	ID      string   `xml:"id,attr"`
+	Data    []Data   `xml:"data"`
+}
+
+type Edge struct {
+	XMLName xml.Name `xml:"edge"`
+	ID      string   `xml:"id,attr"`
+	Source  string   `xml:"source,attr"`
+	Target  string   `xml:"target,attr"`
+	Data    []Data   `xml:"data"`
 }
 
 // Represents a GraphML Document.
 type Document struct {
 	XMLName xml.Name `xml:"graphml"`
 	Keys    []Key    `xml:"key"`
+	Graph   Graph    `xml:"graph"`
 }
 
 func FromFile(path string) (*Document, error) {
