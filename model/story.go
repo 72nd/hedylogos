f9474a0cbc73e211c7453201478b6c6053616f44
72nd <msg@frg72.com>
2022-11-30 17:55:41 +0100

    model package documented

--- model/story.go
+++ model/story.go
@@ -1,1 +1,5 @@
+// The model package contains the definition of the fundamental data types
+// used to represent the structure of the story represented as a graph.
+// This package also handles the import and conversion from the more basic
+// result of an [github.com/hedylogos/graphml] import.l
 package model
