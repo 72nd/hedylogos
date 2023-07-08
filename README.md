dd5d4a5bfe0c9c1564d66fcc6fffcd6213127612
72nd <msg@frg72.com>
2023-07-08 23:27:02 +0200

    readme added

---
+++ README.md
@@ -0,0 +1,8 @@
+# hedylogos – Interactive audio scenarios for old dial telephones
+
+<p align="center">
+  <img src="misc/header.png" />
+</p>
+
+
+Hedylogos allows the creation of interactive audio scenarios using the [yEd graph editor](https://www.yworks.com/products/yed). The scenarios can be played on a old dial phone as well as a normal computer which has a number pad attached. 
--- hedylogos/__main__.py
+++ hedylogos/__main__.py
@@ -1,15 +1,19 @@
-import click
-
 from model import Scenario
 
 from pathlib import Path
+from typing_extensions import Annotated
 
-@click.command()
-@click.option("--input", "-i", "input_file", help="path to input file", required=True)
-def run(input_file: Path):
-    scenario = Scenario(input_file)
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def run(
+    path: Annotated[Path, typer.Argument(help="path to input file")]
+):
+    scenario = Scenario(path)
     print(scenario['title'])
 
 
 if __name__ == "__main__":
-    run()
+    app()
